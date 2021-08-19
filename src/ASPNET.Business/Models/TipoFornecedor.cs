bf7457ff54822becac67411975e6a5dda0653392
joaofernando <joao_jfmx@outlook.com>
2021-08-19 15:09:31 -0300

    Adicionar arquivos de projeto.

---
+++ src/ASPNET.Business/Models/TipoFornecedor.cs
@@ -0,0 +1,7 @@
+﻿namespace ASPNET.Business.Models {
+    public enum TipoFornecedor
+    {
+        PessoaFisica = 1,
+        PessoaJuridica
+    }
+}
---
+++ src/ASPNET.Data/Context/MeuDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ASPNET.Business.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASPNET.Data.Context {
+    public class MeuDbContext : DbContext{
+        public MeuDbContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Endereco> Enderecos { get; set; }
+        public DbSet<Fornecedor> Fornecedores { get; set; }
+
+    }
+}
